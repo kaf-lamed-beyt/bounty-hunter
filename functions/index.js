48ed53cdd19360883e73671ee0bba6fb3fa8b449
kaf-lamed-beyt <belac335@gmail.com>
2023-10-01 14:24:42 +0100

    add netlify aws adapter

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+const {
+  createLambdaFunction,
+  createProbot,
+} = require("@probot/adapter-aws-lambda-serverless");
+
+const appFn = require("../index");
+
+module.exports.handler = createLambdaFunction(appFn, {
+  probot: createProbot(),
+});
